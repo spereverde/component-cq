cba6f29bc5271b939d4680ed9cc34a5d930f16dc
Kim Paulissen <kim.paulissen@kuleuven.be>
2022-04-14 11:51:43 +0200

    initial setup

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { onMount } from "svelte";
+  import { apiData, cqElements } from "./store.js";
+
+  onMount(async () => {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Bourbon")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        apiData.set(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        return [];
+      });
+  });
+  export let cq;
+</script>
+
+<main>
+  <h1>CQ: {cq}</h1>
+  <ul>
+    {#each $cqElements as cqElement}
+      <li>{cqElement}</li>
+    {/each}
+  </ul>
+</main>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import App from "./App.svelte";
+
+const app = new App({
+  target: document.body,
+  props: {
+    name: "KIM",
+    cq: "50268948",
+  },
+});
+
+export default app;
