c4f2454cb55a0f53d57fc0ce3d4e613598426fa4
Michaël Penninck <michael.penninck@kuleuven.be>
2022-04-28 16:35:15 +0200

    make custom element work
    not using the onMount anymore, instead use reactivity to see when cq nr changed and refetch data

--- src/Cq.svelte
+++ src/Cq.svelte
@@ -1,45 +1,70 @@
 <svelte:options tag="cq-component" />
-<!-- <svelte:head>
+<svelte:head>
+    <!-- both this and import at the bottom seems to be needed to make the icons work -->
     <style>
         @import url("https://fonts.googleapis.com/css2?family=Material+Icons&family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Source+Serif+Pro:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap");
     </style>
-</svelte:head> -->
-
+</svelte:head>
 <script>
-    console.log("in script");
-    import { onMount } from "svelte";
     import { apiData, cqElements } from "./store.js";
-  
-    export let cq = "50268948";
-    //export let cq;
-    export let languageSetting = "en";
+
+    export let cq;
+    export let language_setting;
     let language = "";
-    if ((languageSetting = "en")) {
+    $: if ((language_setting == "en")) {
       language = "-en";
     }
   
     let apiBaseUrl = "https://search-es6.q.icts.kuleuven.be/oa";
-    let url = apiBaseUrl + language + "/_doc/" + cq;
-  
-    onMount(async () => {
-        console.log("in onMount");
-      fetch(url)
+    $: url = (cq != null && language != null)? apiBaseUrl + language + "/_doc/" + cq : null;
+
+    $: console.log("url:" + url);
+
+    $: if (url != null){
+        fetch(url)
         .then((response) => response.json())
         .then((data) => {
-          // console.log(data);
+          console.log(data);
           apiData.set(data);
         })
         .catch((error) => {
           console.log(error);
           return [];
         });
-    });
+    }
+
+    // $: console.log("language_setting: "+language_setting);
+    // console.log("cq: "+cq);
+
+    // fetch(url)
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //       console.log(data);
+    //       apiData.set(data);
+    //     })
+  
+    // onMount(async () => {
+    //     await tick();
+    //     console.log("in onMount");
+    //     let url = apiBaseUrl + language + "/_doc/" + cq;
+    //     console.log("in onmount url: "+url);
+    //   fetch(url)
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //       // console.log(data);
+    //       apiData.set(data);
+    //     })
+    //     .catch((error) => {
+    //       console.log(error);
+    //       return [];
+    //     });
+    // });
   </script>
   
 <div>
-    <link rel='stylesheet' href="https://fonts.googleapis.com/css2?family=Material+Icons&family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Source+Serif+Pro:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap">
+    <!-- <link rel='stylesheet' href="https://fonts.googleapis.com/css2?family=Material+Icons&family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Source+Serif+Pro:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap"> -->
 
-    <h1 class="hidden">CQ: {cq}</h1>
+    <h1>CQ: {cq}</h1>
     <div>
         {#each $cqElements as cq}
         {#if cq.programmeTitle}
@@ -61,7 +86,7 @@
     </div>
 </div>
   
-  <style>
-
-  </style>
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=Material+Icons&family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Source+Serif+Pro:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap");
+</style>
   
